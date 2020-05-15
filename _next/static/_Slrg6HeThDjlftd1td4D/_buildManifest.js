3105ce00da28b6bda0c8338d8d247609182c4d9c
timerg <ggteven@gmail.com>
2020-05-15 17:05:52 +0800

    add next

---
+++ _next/static/_Slrg6HeThDjlftd1td4D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/gozima":[a],"/index":[a]}}("static\u002Fchunks\u002Fba44303557daf7fcd176a2021a64ed0bcc83587b.76efa5f1de99d4a5cf02.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
